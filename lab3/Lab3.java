589c4a26173e6f1c5174aaa14b98526ac9d02646
Anaziiii <kavaansraaiia@gmail.com>
2026-03-08 23:04:35 +0200

    lab3

---
+++ lab3/Lab3.java
@@ -0,0 +1,24 @@
+package lab3;
+
+import java.util.Scanner;
+
+public class Lab3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Введіть перше число:");
+        int a = scan.nextInt();
+
+        System.out.println("Введіть друге число:");
+        int b = scan.nextInt();
+
+        System.out.println("Введіть третє число:");
+        int c = scan.nextInt();
+
+        System.out.println("Результат:");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
+
